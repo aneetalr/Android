c061d47e980bf3ceaa63ab904481290dafdaa4d6
Aneeta L R <aneetalrobinson@gmail.com>
2022-10-12 20:50:55 +0530

    prg8

---
+++ CustomToast/app/src/main/java/com/example/customtoast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.customtoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Custom Toast");
+        Button btn = (Button) findViewById(R.id.show);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater li = getLayoutInflater();
+                View layout = li.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_layout));
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setView(layout);
+                toast.show();
+            }
+        });
+    }
+}
